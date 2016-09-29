2d894f802b4f6735b4e439689def6b2f74ec6989
ASD Lab <asdlab4@iMac-de-ASD-5.local>
2016-09-29 07:59:27 -0500

    Bridget

---
+++ db/migrate/20160929124703_create_recipes.rb
@@ -0,0 +1,14 @@
+class CreateRecipes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :recipes do |t|
+      t.string :name
+      t.string :string
+      t.string :ingredient
+      t.text :s
+      t.text :preparation
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
